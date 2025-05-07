7c097a42a7ff66b6dea11ecb6e873c72ae7de1b4
jtechy1 <tech.jmartinez@outlook.com>
2025-05-07 15:40:32 -0700

    hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,17 +14,21 @@ const Header = ({ user }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-3">
       <Link className="navbar-brand" to="/dashboard">QR Scanner</Link>
+
       <button
         className="navbar-toggler"
         type="button"
         onClick={() => setMenuOpen(!menuOpen)}
+        aria-controls="navbarNav"
+        aria-expanded={menuOpen}
+        aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`}>
+      <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
         <ul className="navbar-nav ms-auto">
-          {user && (
+          {user ? (
             <>
               <li className="nav-item">
                 <Link className="nav-link" to="/dashboard" onClick={() => setMenuOpen(false)}>Dashboard</Link>
@@ -33,11 +37,12 @@ const Header = ({ user }) => {
                 <Link className="nav-link" to="/scanner" onClick={() => setMenuOpen(false)}>Scanner</Link>
               </li>
               <li className="nav-item">
-                <button className="btn btn-outline-light ms-3" onClick={handleLogout}>Logout</button>
+                <button className="btn btn-outline-light ms-lg-3 mt-2 mt-lg-0" onClick={handleLogout}>
+                  Logout
+                </button>
               </li>
             </>
-          )}
-          {!user && (
+          ) : (
             <li className="nav-item">
               <Link className="nav-link" to="/login">Login</Link>
             </li>
